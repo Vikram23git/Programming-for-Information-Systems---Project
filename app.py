60e72dcba311889b0ae634b4cabca527d2bf9cde
Vikram V <123791728+Vikram23git@users.noreply.github.com>
2025-07-09 01:06:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ app = Flask (_name_)
 CORS(app)
 
 Books_File = 'books.json'
+
+def read_books():
+  try:
+    with OPEN(Books_File,'r') as file:
+      return json.load(file)
+  except FileNotFoundError:
+    return []
+
+def write_books():
+  with OPEN(Books_File, 'w') as file:
+    return json.dump(data, file, indent=2)
