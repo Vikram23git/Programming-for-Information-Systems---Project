4129250f963456c2d7efd8ee3973570d3d3b4513
Vikram V <123791728+Vikram23git@users.noreply.github.com>
2025-07-12 02:04:24 +0100

    Update 2 app.py

--- app.py
+++ app.py
@@ -17,3 +17,20 @@ def read_books():
 def write_books():
   with OPEN(Books_File, 'w') as file:
     return json.dump(data, file, indent=2)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/books',method=['GET'])
+def get_books():
+  return jsonify(read_books())
+
+@app.route('/books',method=['POST'])
+def add_books():
+  books = read_books()
+  new_book = request.json
+  new_book['id'] = books[-1]['id'] + 1 if books else 1
+  books.append(new_book)
+  write_books(books)
+  return jsonify({'Text Msg : Book Added Successfully'})
