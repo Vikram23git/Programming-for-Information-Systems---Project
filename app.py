7a7e300cce9e268767ae532b59962d485061a549
Vikram V <123791728+Vikram23git@users.noreply.github.com>
2025-07-14 20:26:22 +0100

    Adding update books and delete book methods for Completing CRUD operation

--- app.py
+++ app.py
@@ -34,3 +34,23 @@ def add_books():
   books.append(new_book)
   write_books(books)
   return jsonify({'Text Msg : Book Added Successfully'})
+
+@app.route('/books/<int:book_id>',methods=['PUT'])
+def update_books(book_id):
+  books = read_books()
+  for book in books:
+    if book['id'] == book_id:
+      updated = request.json
+      book.update(updated)
+      write_books(books)
+      return jsonify({'msg': 'Book has been updated'})
+    return jsonify({'error':'Book not found'}),404
+
+@app.route('/books/<int:book_id>',methods=['DELETE'])
+def delete_book(book_id):
+  books = read_books()
+  books = [book for book in books if book['id'] != book_id]
+  write_books(books)
+  return jsonify({'msg':'Book Deleted'})
+
+app.run(debug=True)
